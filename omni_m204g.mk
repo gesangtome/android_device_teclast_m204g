e43283a3ef3ad063c6ce9373301fdeecd41fed75
flowertome <gesangtome@foxmail.com>
2019-01-11 15:40:48 +0800

    Remove inheritance of object common_full_phone.mk
    File common_full_phone.mk does not exist on omnirom
    
    Signed-off-by: flowertome <gesangtome@foxmail.com>

--- omni_m204g.mk
+++ omni_m204g.mk
@@ -22,9 +22,6 @@ $(call inherit-product, vendor/omni/config/cdma.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/omni/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/teclast/m204g/teclast.mk)
 
