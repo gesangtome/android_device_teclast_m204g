8b37d5c336717aadda44e8d19307d23ce51cfa94
flowertome <gesangtome@foxmail.com>
2019-01-11 16:37:29 +0800

    Deletes redundant image copy processes
    Signed-off-by: flowertome <gesangtome@foxmail.com>

--- teclast.mk
+++ teclast.mk
@@ -17,16 +17,6 @@ $(call inherit-product-if-exists, vendor/teclast/m204g/m204g--vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/teclast/m204g/overlay
 
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/teclast/m204g/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
