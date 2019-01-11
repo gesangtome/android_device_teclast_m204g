60a4866c68fd542fbca115dd0594edd50a9320e7
flowertome <gesangtome@foxmail.com>
2019-01-11 15:32:14 +0800

    Remove inheritance of object languages_full.mk
    Signed-off-by: flowertome <gesangtome@foxmail.com>

--- teclast.mk
+++ teclast.mk
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
