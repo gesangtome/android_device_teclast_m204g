f1afe27c603fe49431058abac5bf316b776fcffe
flowertome <gesangtome@foxmail.com>
2019-04-20 06:11:01 +0800

    mkvendor.sh: Initial commit
    Signed-off-by: flowertome <gesangtome@foxmail.com>

---
+++ omnirom.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2017-2019 Flowertome
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Release name
+PRODUCT_RELEASE_NAME := Teclast M204G
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/Teclast/M204G/teclast.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := M204G
+PRODUCT_NAME := omni_M204G
+PRODUCT_BRAND := Teclast
+PRODUCT_MODEL := M204G
+PRODUCT_MANUFACTURER := Teclast
