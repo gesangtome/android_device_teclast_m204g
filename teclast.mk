cbf66fc8878f6b1005d4498c2de3e47bafa5b1a9
flowertome <gesangtome@foxmail.com>
2019-04-20 04:35:37 +0800

    Remove redundant attributes
    Signed-off-by: flowertome <gesangtome@foxmail.com>

--- teclast.mk
+++ teclast.mk
@@ -17,8 +17,3 @@ $(call inherit-product-if-exists, vendor/teclast/m204g/m204g--vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/teclast/m204g/overlay
 
-$(call inherit-product, build/target/product/full.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_m204g
-PRODUCT_DEVICE := m204g
