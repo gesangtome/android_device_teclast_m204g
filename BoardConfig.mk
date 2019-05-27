f1d9406356305db446ca4e2f207e5a8909de5b94
flowertome <gesangtome@foxmail.com>
2019-05-27 14:08:16 +0800

    mkvendor.sh: initial commit
    Signed-off-by: flowertome <gesangtome@foxmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2018 The Android Open Source Project
+# Copyright (C) 2019 The AueUI Project
+# Copyright (C) 2018-2019 flowertome
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/Teclast/M204G/BoardConfigVendor.mk
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := M204G
+
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 buildvariant=user
+BOARD_KERNEL_BASE := 0x40080000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/Teclast/M204G/kernel
+
+BOARD_HAS_NO_SELECT_BUTTON := true
---
+++ README.md
@@ -0,0 +1,26 @@
+# Device tree for teclast m204g
+
+ Copyright (C) 2018 The Android Open Source Project 
+ Copyright (C) 2019 The AueUI Project 
+ Copyright (C) 2018-2019 flowertome 
+
+Maintainer: flowertome <gesangtome@foxmail.com> 
+
+## Device specifications
+
+| Device                  | Teclast M204G                                               |
+| ----------------------- | :---------------------------------------------------------- |
+| SoC                     | MediaTek Helio X20 (MT6797)                                 |
+| Core                    | 2x Cortex-A72 + 4x Cortex-A53 + 4x Cortex-A53               |
+| GPU                     | ARM Mali-T880MP4                                            |
+| Memory                  | 4GB RAM                                                     |
+| Shipped Android version | Android Oreo (8.0.0)                                        |
+| Storage                 | 64GB eMMC flash storage                                     |
+| MicroSD                 | Micro SD Card is supported                                  |
+| Battery                 | Non-removable Li-Po 6600mAh                                 |
+| Display                 | 2560 x 1600                                                 |
+| Size                    | 240mm(L) × 170mm(W) × 10mm(H)                               |
+
+## Device picture
+
+![Teclast M204G Picture](http://www.teclast.com/zt/M204G/images/ztpic/1.jpg)
